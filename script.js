7022f0ea7591aca37f795c20da864b9411fff718
mihaipaulorban <mihaipaulorban@gmail.com>
2023-11-21 16:53:21 +0100

    fix: immagini + autoplay button

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 //OGGETTI IMMAGINE
 const pics = [
     {
-        image: 'img/01.jpg',
+        image: 'img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morales',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
-        image: 'img/02.jpg',
+        image: 'img/02.jpeg',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     }, {
-        image: 'img/03.jpg',
+        image: 'img/03.jpeg',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     }, {
-        image: 'img/04.jpg',
+        image: 'img/04.png',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
     }, {
-        image: 'img/05.jpg',
+        image: 'img/05.jpeg',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
@@ -113,3 +113,32 @@ thumbnailImages.forEach((thumb, i) => {
 });
 
 
+let autoplayInterval;
+
+// Funzione per avviare l'autoplay
+function startAutoplay() {
+    autoplayInterval = setInterval(() => {
+        changePic('next'); 
+    }, 3000);
+}
+
+// Funzione per fermare l'autoplay
+function stopAutoplay() {
+     // Interrompe l'intervallo di autoplay
+    clearInterval(autoplayInterval);
+}
+
+// Avvio dell'autoplay tramite bottone
+const autoplayButton = document.getElementById('autoplay');
+
+autoplayButton.addEventListener('click', function () {
+    const isAutoplayOn = autoplayButton.classList.toggle('active');
+
+    if (isAutoplayOn) {
+         // Avvia l'autoplay se il pulsante è attivo
+        startAutoplay();
+    } else {
+         // Altrimenti, ferma l'autoplay
+        stopAutoplay();
+    }
+});
